ca1c5c306d6b98c85dbb34ecfe670210267e73fe
jonathan-perucca <under.64@gmail.com>
2019-05-12 16:42:21 +0200

    minor renaming to avoid maven naming conflict

--- message-bus-worker-queues/build-and-run-jib-dockercompose.sh
+++ message-bus-worker-queues/build-and-run-jib-dockercompose.sh
@@ -2,7 +2,8 @@
 
 ./cleanup.sh
 
-mvn compile jib:dockerBuild -f gamer-creator/pom.xml
-mvn compile jib:dockerBuild -f gamer-emailer/pom.xml
+mvn compile jib:dockerBuild -f gamer-creator-worker-queues/pom.xml
+mvn compile jib:dockerBuild -f gamer-emailer-worker-queues/pom.xml
 
-docker-compose up
+docker-compose up -d
+docker-compose logs -f gamer-emailer-one-worker-queues gamer-emailer-two-worker-queues
